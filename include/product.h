37c6970e23ea7768349885371ed83aba4fa1e268
danielzontaojeda <danielzontaojeda@gmail.com>
2022-10-26 10:37:22 -0300

    finished refactoring

--- include/product.h
+++ include/product.h
@@ -1,22 +1,23 @@
 #ifndef __PRODUCT_H
 #define __PRODUCT_H
 
-// #include "linked_list.h"
+#define SIZE_NAME sizeof(char)*50
+#define SIZE_DESCRIPTION sizeof(char)*200
 
 // significado das escolhas recebidas
 enum choice_enum {create = 1, print};
 
 // estrutura de no para os produtos
 typedef struct product{
-	char type[2]; 			// tipo do produto (SD, BB, EX, SM ou PD)
-	int id;					// id no produto no arquivo
-	char name[50];			// nome do produto
-	char description[200];	// descricao do produto apenas para sanduiches
-	int is_available;		// 1 = disponivel, 0 = nao disponivel
-	double price[2];		// preco[0] = pequeno, preco[1] = medio, preco[2] = grande
-							// preco de produtos sem tamanho vao em preco[0]
-	int next;				// endereco no arquivo do proximo elemento da lista
-							// -1 caso nao exista proximo
+	char type[2]; 						// tipo do produto (SD, BB, EX, SM ou PD)
+	int id;								// id no produto no arquivo
+	char name[SIZE_NAME];				// nome do produto
+	char description[SIZE_DESCRIPTION];	// descricao do produto apenas para sanduiches
+	int is_available;					// 1 = disponivel, 0 = nao disponivel
+	double price[2];					// preco[0] = pequeno, preco[1] = medio, preco[2] = grande
+										// preco de produtos sem tamanho vao em preco[0]
+	int next;							// endereco no arquivo do proximo elemento da lista
+										// -1 caso nao exista proximo
 } Product;
 
 // Imprime informacoes do produto na tela
@@ -31,7 +32,7 @@ void print_product(Product* product);
 // Retorno: estrutura de produto 
 // Pré-condições: tipo do produto valido
 // Pós-condições: estrutura de produto retornada
-Product* create_product(char* type);
+Product* get_product_keyboard(const char* type);
 
 // Executa o fluxo do programa correto para a escolha realizada
 // Entrada: escolha selecionada e tipo do produto
@@ -40,4 +41,5 @@ Product* create_product(char* type);
 // Pós-condições: nenhum
 void process_submenu_choice(int choice, char* type);
 
+Product* create_product(char* type, char* name, char* description, int is_available, double* price);
 #endif
--- src/product.c
+++ src/product.c
@@ -5,46 +5,51 @@
 #include "product.h"
 #include "util.h"
 
-static void get_name(Product* product){
+static char* get_name(){
+	char* name = malloc(SIZE_NAME);
 	printf("Digite o nome do produto.\n");
-	fgets(product->name, sizeof(product->name), stdin);
-	remove_new_line(product->name);
+	fgets(name, SIZE_NAME, stdin);
+	remove_new_line(name);
+	return name;
 }
 
-static void get_description(Product* product){
-	if(strcmp(product->type, "SD") == 0){
+static char* get_description(const char* type){
+	char* description = malloc(SIZE_DESCRIPTION);
+	if(strcmp(type, "SD") == 0){
 		printf("Digite a descricao do produto.\n");
-		fgets(product->description, sizeof(product->description), stdin);
-		remove_new_line(product->description);
+		fflush(stdin);
+		fgets(description, SIZE_DESCRIPTION, stdin);
+		remove_new_line(description);
 	}
+	return description;
 }
 
-static void get_availability(Product* product){
+static int get_availability(){
+	int is_available;
 	printf("Produto disponivel? (1 = SIM, 0 = NAO)\n");
-	scanf("%d%*c",&product->is_available);
+	scanf("%d%*c",&is_available);
+	return is_available;
 }
 
-static void get_price(Product* product){
-	double price[2];
+static double* get_price(const char* type){
+	double* price = malloc(sizeof(double)*2);
 	char temp[200];
 	char price_str[2][10];
-	if(!strcmp(product->type, "SD") || !strcmp(product->type, "BB")){
+	if(!strcmp(type, "SD") || !strcmp(type, "BB")){
 		printf("Digite os precos para o produto pequeno, medio e grande.\n");
 		fgets(temp, sizeof(temp), stdin);
 		sscanf(temp, "%[^;];%[^;];%[^\n]%*c", price_str[0], price_str[1], price_str[2]);
 		for(int i=0;i<3;i++){
 			replace_char(price_str[i], ',', '.');
 			price[i] = strtod(price_str[i], NULL);
-			product->price[i] = price[i];
-			printf("%lf\n",price[i]);
 		}
 	}else{
 		printf("Digite o preco do produto.\n");
 		fgets(temp, sizeof(temp), stdin);
 		replace_char(temp, ',', '.');
 		price[0] = strtod(temp, NULL);
-		product->price[0] = price[0];
 	}
+	return price;
 }
 
 void print_product(Product* product){
@@ -65,33 +70,40 @@ void print_product(Product* product){
 	printf("-------------------------------------------\n");
 }
 
-Product* create_product(char* type){
-	Product* product = malloc(sizeof(Product));
-	strcpy(product->type, type);
-	get_name(product);
-	get_description(product);
-	get_availability(product);
-	get_price(product);
+Product* get_product_keyboard(const char* type){
+	char* name = get_name();
+	char* description = get_description(type);
+	int is_available = get_availability();
+	double* price = get_price(type);
+	Product* product = create_product(type, name, description, is_available, price);	
+	free(name);
+	free(description); 
+	free(price);
 	print_product(product);
 	return product;
 }
 
-Product* create_product(char* name, char* description, 
-						int availability, double price){
+Product* create_product(char* type, char* name, char* description, 
+						int is_available, double* price){
 	Product* product = malloc(sizeof(Product));
 	strcpy(product->type, type);
-	get_name(product);
-	get_description(product);
-	get_availability(product);
-	get_price(product);
-	print_product(product);
+	strcpy(product->name, name);
+	strcpy(product->description, description);
+	product->is_available = is_available;
+	if(!strcmp(type, "SD") || !strcmp(type, "BB")){
+		for(int i=0; i<3; i++)
+			product->price[i] = price[i];
+	}else{
+		product->price[0] = price[0];
+	}
 	return product;
+
 }
 
 
 void process_submenu_choice(int choice, char* type){
 	if(choice == create){
-		Product* product = create_product(type);
+		Product* product = get_product_keyboard(type);
 		//TODO: gravar em arquivo
 	}
 	//TODO: imprimir todos
