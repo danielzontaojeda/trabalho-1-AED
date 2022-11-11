80c35dc3d1b30ab2e72df67843c5fce835261714
danielzontaojeda <danielzontaojeda@gmail.com>
2022-11-11 17:58:50 -0300

    binary_file docs

--- include/binary_file.h
+++ include/binary_file.h
@@ -8,25 +8,64 @@
 
 #define DATABASE_FOLDER "./database"
 
+// Cabecalho para a funcao do sistema para criar uma pasta
+// Entrada: caminho da pasta para ser criada
+// Retorno: 1 caso a criacao tenha sucedido, caso contrario 0
+// Pré-condições: nenhuma
+// Pós-condições: nenhuma
 int _mkdir(const char *pathname);
 
+// Cria stream para banco de dados de produtos caso ele nao exista
+// e retorna um ponteiro para tal
+// Entrada: nenhuma
+// Retorno: pointeiro para stream do arquivo de banco de dados dos produtos
+// Pré-condições: nenhuma
+// Pós-condições: ponteiro para o arquivo retornado
 FILE *get_database_product();
 
-FILE *get_database_completed_orders();
-
 // Cria pasta 'database' se ela nao existir
-// Entrada: Nenhuma
-// Retorno: Nenhum
-// Pré-condições: Nenhuma
-// Pós-condições: Pasta 'database' criada
+// Entrada: nenhuma
+// Retorno: nenhum
+// Pré-condições: nenhuma
+// Pós-condições: pasta 'database' criada
 void create_database_directory();
 
+// Coloca todos os produtos na lista encadeada no banco de dados de produtos
+// Entrada: lista encadeada com produtos
+// Retorno: nenhum
+// Pré-condições: list_products->info e um produto
+// Pós-condições: todos os produtos inseridos no banco de dados
 void write_product_list_to_file(LinkedList *list_products);
 
+// Coloca product em database_product
+// Entrada: estrutura de produtos e ponteiro para arquivo
+// Retorno: nenhum
+// Pré-condições: database_product e um ponteiro valido
+// Pós-condições: produto inserido no primeiro lugar vazio do banco de dados
 void write_product_to_file(Product *product, FILE *database_product);
 
+// Retorna 1 caso o arquivo esteja vazio, caso contrario retorna 0
+// Entrada: ponteiro para o arquivo
+// Retorno: 1 ou 0
+// Pré-condições: ponteiro para o arquivo valido
+// Pós-condições: inteiro booleano retornado 
 int is_file_empty(FILE *database);
 
+// Imprime em stdout todos os produtos do tipo type em database_product
+// Entrada: ponteiro para o banco de dados de produtos
+//        	tipo do produto
+// Retorno: nenhum
+// Pré-condições: type e um tipo de produto valido
+//                database_product e um ponteiro valido
+// Pós-condições: todos os produtos do tipo type impressos em stdout 
 void print_all_products_in_file(FILE *database_product, char *type);
+
+// Retorna produto da posicao pos no arquivo database_product
+// Entrada: ponteiro para o arquivo de banco de dados dos produtos
+// 			posicao no arquivo em que o produto desejado se encontra
+// Retorno: estrutura do produto
+// Pré-condições: um produto existe nessa posicao
+// Pós-condições: estrutura do produto retornada
+Product *read_node(FILE *database_product, int pos);
 
 #endif
--- src/binary_file.c
+++ src/binary_file.c
@@ -8,6 +8,12 @@
 #include "debug.h"
 #include "header.h"
 
+// Cria stream para banco de dados de produtos caso ele nao exista
+// e retorna um ponteiro para tal
+// Entrada: nenhuma
+// Retorno: pointeiro para stream do arquivo de banco de dados dos produtos
+// Pré-condições: nenhuma
+// Pós-condições: ponteiro para o arquivo retornado
 FILE *get_database_product(){
 	char filename[100];
 	strcpy(filename, DATABASE_FOLDER);
@@ -18,12 +24,23 @@ FILE *get_database_product(){
 	return file;
 }
 
+// Retorna 1 caso o arquivo esteja vazio, caso contrario retorna 0
+// Entrada: ponteiro para o arquivo
+// Retorno: 1 ou 0
+// Pré-condições: ponteiro para o arquivo valido
+// Pós-condições: inteiro booleano retornado
 int is_file_empty(FILE *database){
+	assert(database);
 	fseek(database, 0, SEEK_END);
 	int is_empty = ftell(database) == 0;
 	return is_empty;
 }
 
+// Cria pasta 'database' se ela nao existir
+// Entrada: Nenhuma
+// Retorno: Nenhum
+// Pré-condições: Nenhuma
+// Pós-condições: Pasta 'database' criada
 void create_database_directory(){
 	#ifdef __linux__
        mkdir(DATABASE_FOLDER, 777); 
@@ -32,18 +49,30 @@ void create_database_directory(){
    #endif
 }
 
+// Retorna produto da posicao pos no arquivo database_product
+// Entrada: ponteiro para o arquivo de banco de dados dos produtos
+// 			posicao no arquivo em que o produto desejado se encontra
+// Retorno: estrutura do produto
+// Pré-condições: um produto existe nessa posicao
+// Pós-condições: estrutura do produto retornada
 Product *read_node(FILE *database_product, int pos){
 	if(pos == EMPTY) return NULL;
 	Product *product = malloc(sizeof(Product));
 	fseek(database_product, sizeof(Header) + pos*sizeof(Product), SEEK_SET);
 	fread(product, sizeof(Product), 1, database_product);
-	// #ifdef __DEBUG
-	// 	print_product(product);
-	// #endif
 	return product;
 }
 
+// Imprime em stdout todos os produtos do tipo type em database_product
+// Entrada: ponteiro para o banco de dados de produtos
+//        	tipo do produto
+// Retorno: nenhum
+// Pré-condições: type e um tipo de produto valido
+//                database_product e um ponteiro valido
+// Pós-condições: todos os produtos do tipo type impressos em stdout 
 void print_all_products_in_file(FILE *database_product, char *type){
+	assert(is_type_of_product(type));
+	assert(database_product);
 	Header *header = read_header(database_product);
 	Product *product = read_node(database_product, header->pos_head);
 	if(product == NULL) printf("Nenhum produto encontrado.\n");
@@ -57,6 +86,11 @@ void print_all_products_in_file(FILE *database_product, char *type){
 	free(header);
 }
 
+// Coloca todos os produtos na lista encadeada no banco de dados de produtos
+// Entrada: lista encadeada com produtos
+// Retorno: nenhum
+// Pré-condições: list_products->info e um produto
+// Pós-condições: todos os produtos inseridos no banco de dados
 void write_product_list_to_file(LinkedList *list_products){
 	assert(list_products);
 	int size = 0;
@@ -76,6 +110,11 @@ void write_product_list_to_file(LinkedList *list_products){
 	fclose(database_product);
 }
 
+// Coloca product em database_product
+// Entrada: estrutura de produtos e ponteiro para arquivo
+// Retorno: nenhum
+// Pré-condições: database_product e um ponteiro valido
+// Pós-condições: produto inserido no primeiro lugar vazio do banco de dados
 static void insert_product(Product *product, FILE *database_product){
 	Header *header = read_header(database_product);
 	assert(header);
@@ -97,6 +136,11 @@ static void insert_product(Product *product, FILE *database_product){
 	free(header);
 }
 
+// Coloca product em database_product
+// Entrada: estrutura de produtos e ponteiro para arquivo
+// Retorno: nenhum
+// Pré-condições: database_product e um ponteiro valido
+// Pós-condições: produto inserido no primeiro lugar vazio do banco de dados
 void write_product_to_file(Product *product, FILE *database_product){
 	#ifdef __DEBUG
 		printf("DEBUG: write_product_to_file -> product.name = %s, product.id = %d\n",product->name, product->id);		
