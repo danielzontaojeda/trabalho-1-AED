5b0ee127fc76b8e511d99d371c1b5a7fadf68b7c
danielzontaojeda <danielzontaojeda@gmail.com>
2022-11-25 16:55:26 -0300

    created order.c

---
+++ src/order.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+#include "linked_list.h"
+#include "order.h"
+
+LinkedList *create_order_from_file(LinkedList *commands){
+
+}
